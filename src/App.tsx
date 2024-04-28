4a5ab2f767f41f1505b65b3edf82b0f5edd11956
Z1yoda <ziyoda01092007@gmail.comgit init git config  --global user.email ziyoda01092007@gmil.com>
2024-04-28 17:32:12 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,45 @@
+import "./App.css";
+import {
+  YMaps,
+  Map,
+  GeolocationControl,
+  FullscreenControl,
+  RouteButton,
+  RouteEditor,
+  RulerControl,
+  TrafficControl,
+  SearchControl,
+  ZoomControl,
+  TypeSelector,
+} from "@pbe/react-yandex-maps";
+
+function App() {
+
+  return (
+    <div className="map-container">
+      <YMaps query={{ apikey: "949d2a1a-b458-48e9-b14a-8f0cf25ddc44" }}>
+        <Map
+          height={'100%'}
+          width={'100%'}
+          defaultState={{
+            center: [55.75, 37.57],
+            zoom: 10,
+            controls: [],
+          }}
+        >
+          <GeolocationControl options={{ float: "left" }} />
+          <FullscreenControl options={{ float: "left" }} />
+          <RouteButton options={{ float: "right" }} />
+          <RouteEditor options={{ float: "right" }} />
+          <RulerControl options={{ float: "right" }} />
+          <SearchControl options={{ float: "right" }} />
+          <ZoomControl options={{ float: "right" }} />
+          <TypeSelector options={{ float: "right" }} />
+          <TrafficControl options={{ float: "right" }} />
+        </Map>
+      </YMaps>
+      </div>
+  );
+}
+
+export default App;
